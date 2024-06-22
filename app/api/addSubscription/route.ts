26f15e80cfae144d1d5322de6d8114365bdf5bb2
Rouven Spaar <rouven.spaar@gmail.com>
2024-06-22 11:13:23 +0200

    removed edge runtime

--- app/api/addSubscription/route.ts
+++ app/api/addSubscription/route.ts
@@ -2,46 +2,24 @@ import mailchimp from "@mailchimp/mailchimp_marketing";
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_API_SERVER, // e.g. us1
+  server: process.env.MAILCHIMP_API_SERVER,
 });
 
-export const runtime = 'edge';
-
 export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  if (!email) new Response(JSON.stringify({ error: "Email is required" }));
+
   try {
-    const { email } = await request.json();
-
-    if (!email) {
-      return new Response(
-        JSON.stringify({ error: "Email is required" }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
     const res = await mailchimp.lists.addListMember(
       process.env.MAILCHIMP_AUDIENCE_ID!,
       { email_address: email, status: "subscribed" }
     );
 
-    return new Response(
-      JSON.stringify({ message: 'Subscription added successfully', res }),
-      { status: 200, headers: { "Content-Type": "application/json" } }
-    );
+    return new Response(JSON.stringify({ res }));
   } catch (error: any) {
-    let errorMessage = 'Internal Server Error';
-
-    if (error.response && error.response.text) {
-      try {
-        const parsedError = JSON.parse(error.response.text);
-        errorMessage = parsedError.detail || parsedError.title || errorMessage;
-      } catch (e) {
-        // If parsing fails, keep the generic error message
-      }
-    }
-
     return new Response(
-      JSON.stringify({ error: errorMessage }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
+      JSON.stringify({ error: JSON.parse(error.response.text) })
     );
   }
 }
